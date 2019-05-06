46b4403cd631e186f9bf4869211693d47fd025b3
cheyxx <fun.colin.he@gmail.com>
2019-05-06 18:05:51 +0800

    kmp algorithm

---
+++ kmp.hh
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<memory>
+using namespace std;
+class KMP
+{
+public:
+    KMP(string match, string original);
+    int algorithm();
+    void calculate_next();
+
+private:
+    string m_match;
+    string m_original;
+    std::shared_ptr<int> m_next;
+};
+
+
