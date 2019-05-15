142b61190a3fa774730b476e804ffb1e47851d85
cheyxx <fun.colin.he@gmail.com>
2019-05-15 21:33:59 +0800

    morris travarsal

---
+++ src/morris_traversal/morris_traversal.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "morris_traversal.hh"
+using namespace std;
+
+/*非递归， 非栈 二叉树中序遍历 */
+void
+inorder_morris_traversal(struct node *root){
+    struct node *curr = root;
+    struct node *prev;
+    while(!curr){
+        if (curr->left == nullptr){
+            curr = curr->right;
+        }
+        else
+        {
+            prev = curr->left;
+            while((prev->right != nullptr) && (prev->right != curr)) {
+                prev = prev->right;
+            }
+            if (prev->right ==nullptr){
+                prev->right = curr;
+                curr = curr->left;
+            }
+            else
+            {
+                prev->right = nullptr;
+                std::cout << "value " << curr->value << std::endl;
+                curr = curr->right;
+            }
+        }
+
+    } 
+
+}
