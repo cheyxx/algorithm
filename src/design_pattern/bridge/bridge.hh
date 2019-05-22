bc3cf882c553dae3e43c70e2a55b7817b358abbe
cheyxx <fun.colin.he@gmail.com>
2019-05-22 20:39:33 +0800

    bridge

---
+++ src/design_pattern/bridge/bridge.hh
@@ -0,0 +1,54 @@
+#ifndef BRIDE_HH_
+#define BRIDE_HH_
+#include <iostream>
+#include <memory>
+
+class TV {
+public:
+    virtual void on() = 0;
+    virtual void off() = 0;
+    virtual void tuneChannel() = 0; 
+};
+
+class Sony : public TV {
+public:
+    void on();
+    void off();
+    void tuneChannel();
+};
+
+class RCA : public TV {
+public:
+    void on();
+    void off();
+    void tuneChannel();
+};
+
+class RemoteControl {
+public:
+    virtual void on() = 0;
+    virtual void off() = 0;
+    virtual void setChannel() = 0;
+};
+
+class ConcreteRemoteLaser: public RemoteControl {
+public:
+
+     ConcreteRemoteLaser(std::shared_ptr<TV> impl);
+     void on();
+     void off();
+     void setChannel(); 
+private:
+    std::shared_ptr<TV> _impl;
+};
+
+class ConcreteRemoteInfrareRay: public RemoteControl {
+public:
+     ConcreteRemoteInfrareRay(std::shared_ptr<TV> impl);
+     void on();
+     void off();
+     void setChannel();
+private:
+    std::shared_ptr<TV> _impl;
+};
+#endif
