5b69bbb6ba44102dd50153338e4d919ef7e7b4d6
cheyxx <fun.colin.he@gmail.com>
2019-06-07 14:32:06 +0800

    cmake format

---
+++ main.cc
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <iostream>  // std::cin, std::cout
+#include <iterator>  // std::istream_iterator
+#include <memory>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include "patterns/Singleton.h"
+#include "patterns/adapter.h"
+#include "patterns/adapter.h"
+#include "patterns/bridge.h"
+#include "patterns/builder.h"
+#include "patterns/composite.h"
+#include "patterns/factory.h"
+#include "patterns/filter.h"
+#include "algorithms/kmp.h"
+#include "algorithms/multiple.h"
+#include "patterns/observer.h"
+#include "algorithms/pid.h"
+#include "patterns/single_factory.h"
+#include "patterns/strategy.h"
+#include "patterns/abstract_factory.h"
+#include "algorithms/reverse_polish_notation.h"
+
+int main() {
+	
+}
